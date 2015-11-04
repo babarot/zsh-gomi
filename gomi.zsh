0f73986ba66363bf9242dc65b97d0286d4273f30
b4b4r07 <b4b4r07@gmail.com>
2015-11-04 17:22:39 +0900

    Refactor and format

--- gomi.zsh
+++ gomi.zsh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
-if [[ -n $ZSH_VERSION ]] && (( $+commands[fzf] )); then
-    export PATH=$PATH:${0:A:h}/bin
-    fpath=(${0:A:h}/src(N-/) $fpath)
+# zsh script
+if [[ -n $ZSH_VERSION ]]; then
+
+    # At first, you should install junegunn/fzf
+    if (( $+commands[fzf] )); then
+
+        # Add to the PATH
+        export PATH=$PATH:${0:A:h}/bin
+        fpath=(${0:A:h}/src(N-/) $fpath)
+    fi
 fi
